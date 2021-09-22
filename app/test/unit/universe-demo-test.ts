50fc874caa48d14dcdb76aa822fe929a5eed05ce
tidy-dev <tidy-dev@github.com>
2021-09-22 13:19:43 -0400

    Add describeProgrammingLanguages Unit tests

--- app/test/unit/universe-demo-test.ts
+++ app/test/unit/universe-demo-test.ts
@@ -5,3 +5,15 @@ enum ProgrammingLanguage {
   CSS,
   Java,
 }
+
+describe('universe-demo-describeProgrammingLanguage', () => {
+  it('it describes javascript', () => {
+    const result = describeProgrammingLanguage(ProgrammingLanguage.JavaScript)
+    expect(result).toBe('Yeah.. It is pretty good.')
+  })
+
+  it('it can not descripe java', () => {
+    const result = describeProgrammingLanguage(ProgrammingLanguage.Java)
+    expect(result).toBe(`Don't know what you are talking about..`)
+  })
+})
